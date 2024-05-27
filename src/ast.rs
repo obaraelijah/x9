c9994d55e5bc6a7635f67cd885ec56da3ddcc179
obaraelijah <elijahobara357@gmail.com>
2024-05-27 20:47:06 +0300

    parse logic

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,4 @@
-use bigdecimal::BigDecimal;
+use bigdecimal::{BigDecimal, FromPrimitive, ToPrimitive};
 use std::sync::Arc;
 
 pub type Integer = i64;
@@ -32,6 +32,59 @@ impl std::fmt::Debug for Expr {
             Expr::Symbol(s) => write!(f, "{}", s),
         }
     }
+}
+
+pub(crate) trait ToNumericExpr {
+    fn to_expr(self) -> Expr;
+    fn to_bigdecimal(self) -> Num;
+}
+
+impl ToNumericExpr for usize {
+    fn to_expr(self) -> Expr {
+        match self.try_into() {
+            Ok(res) => Expr::Integer(res),
+            _ => Expr::Num(FromPrimitive::from_usize(self).unwrap()),
+        }
+    }
+
+    fn to_bigdecimal(self) -> Num {
+        FromPrimitive::from_usize(self).unwrap()
+    }
+}
+
+impl ToNumericExpr for Integer {
+    fn to_expr(self) -> Expr {
+        
+        Expr::Integer(self)
+    }
+
+    fn to_bigdecimal(self) -> Num {
+        FromPrimitive::from_i64(self).unwrap()
+    }
+}
+
+impl ToNumericExpr for BigDecimal {
+    fn to_expr(self) -> Expr {
+        if self.is_integer() {
+            match self.to_i64() {
+                Some(i) => Expr::Integer(i),
+                None => Expr::Num(self),
+            }
+        } else {
+            Expr::Num(self)
+        }
+    }
+
+    fn to_bigdecimal(self) -> Num {
+        self
+    }
+}
+
+
+impl Expr {
+    pub(crate) fn num<T: ToNumericExpr>(number: T) -> Self {
+        number.to_expr()
+    } 
 }
 
 pub type LispResult<T> = anyhow::Result<T>;
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,27 @@
+use anyhow::anyhow;
+
 use crate::ast::{Expr, Num, Integer, LispResult};
 
-pub fn parse_num(input: &str) -> LispResult<Expr, String> {
-    todo!()
+pub fn parse_num(input: &str) -> LispResult<(Expr, usize)> {
+    let next_whitespace_or_end_of_string = input
+        .chars()
+        .position(|c| !(c == '.' || c.is_numeric() || c == '-'))
+        .unwrap_or(input.len());
+
+    //  slices the input string from the start to the position found in the previous step. 
+    let input = &input[0..next_whitespace_or_end_of_string];
+
+    if let Ok(res) = input.parse::<Integer>() {
+        return Ok((Expr::num(res), next_whitespace_or_end_of_string));
+    }
+
+    if let Ok(res) = input.parse::<Num>() {
+        return Ok((Expr::num(res), next_whitespace_or_end_of_string));
+    }
+    Err(anyhow!("Cannot convert: \"{}\" into an int", input))
 }
 
-fn parse_expr(input: &str) -> LispResult<Expr, usize> {
+
+fn parse_expr(input: &str) -> LispResult<(Expr, usize)> {
     todo!()
 }
