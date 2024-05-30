82b41d7cd2a1a40dcb2b07f9b38864274bd735aa
obaraelijah <elijahobara357@gmail.com>
2024-05-30 19:17:00 +0300

    feat: Interners implimentation

--- src/interner.rs
+++ src/interner.rs
@@ -1,9 +1,12 @@
+use once_cell::sync::Lazy;
+use parking_lot::RwLock;
+
 #[derive(Clone)]
 pub struct InternedString(u32);
 
 impl InternedString {
     pub(crate) fn new(s: String) -> Self {
-        Self(())
+        INTERNER.write().intern(s)
     }
 }
 
@@ -15,6 +18,25 @@ impl std::hash::Hash for InternedString {
 
 impl From<&str> for InternedString {
     fn from(s: &str) -> Self {
-        InternedString::new()
+        InternedString::new(s.to_string())
+    }
+}
+
+static INTERNER: Lazy<RwLock<Interner>> = Lazy::new(|| RwLock::new(Interner::new()));
+
+struct Interner {
+    strings: Vec<String>,
+}
+
+impl Interner {
+    fn new() -> Self {
+        let strings = vec!["".into()];
+        Interner { 
+            strings,
+        }
+    }
+
+    fn intern(&mut self, s: String) -> InternedString {
+        todo!()
     }
 }
