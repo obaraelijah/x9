b8e8f40f18c89738e823f7a07028b7d4ed43a667
obaraelijah <elijahobara357@gmail.com>
2024-06-01 18:59:56 +0300

    feat: Cli parser options

--- src/cli.rs
+++ src/cli.rs
@@ -3,12 +3,24 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "x9", about = "x9 Programming Language")]
 pub struct Options {
+    pub files: Vec<String>,
+    #[structopt(
+        short = "e",
+        long,
+        help = "Execute the file(s), and then load the interpreter"
+    )]
+    pub load_file: bool,
+
     #[structopt(short = "d", long = "debugger", help = "WIP: :^)")]
     pub debugger: bool,
 }
 
 impl Default for Options {
     fn default() -> Self {
-        Options { debugger: false }
+        Options { 
+            files: Vec::with_capacity(0),
+            load_file: false,
+            debugger: false, 
+        }
     }
 }
