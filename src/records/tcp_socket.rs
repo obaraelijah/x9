76185a066ccb3be5b86cd532d0459ec7a05eeaa0
obaraelijah <elijahobara357@gmail.com>
2024-06-09 18:35:08 +0300

    feat: Tcp setup

--- src/records/tcp_socket.rs
+++ src/records/tcp_socket.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use crate::ast::LispResult;
+
 pub(crate) struct TcpListenerRecord {
     tcp_listener: Option<TcpListener>,
     id: u64,
@@ -11,5 +13,15 @@ impl Default for TcpListenerRecord {
             tcp_listener: None, 
             id: rand::random(), 
         }
+    }
+}
+
+impl TcpListenerRecord {
+    fn bind() -> LispResult<Self> {
+        todo!()
+    }
+
+    fn handle_incoming() -> LispResult<()> {
+        todo!()
     }
 }
