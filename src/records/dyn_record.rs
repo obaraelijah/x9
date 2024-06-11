8a2a5e8055787eb93da5452f863d9cdb7ffa453f
obaraelijah <elijahobara357@gmail.com>
2024-06-11 19:27:48 +0300

    chore: Dynamic record logic

---
+++ src/records/dyn_record.rs
@@ -0,0 +1,5 @@
+use crate::ast::Symbol;
+
+pub struct DynRecord {
+    name: Symbol,
+}
--- src/records/mod.rs
+++ src/records/mod.rs
@@ -3,6 +3,7 @@ mod file;
 mod record;
 mod set;
 mod tcp_socket;
+mod dyn_record;
 
 pub(crate) use self::record::{Record, RecordType};
 pub(crate) use self::set::SetRecord;
