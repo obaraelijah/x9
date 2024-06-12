ba284a8b58fb35ed7effbc10bb049c78c3447ac4
obaraelijah <elijahobara357@gmail.com>
2024-06-12 21:01:08 +0300

    feat: Add partial syntax  entry api for DictMut

--- src/records/dict.rs
+++ src/records/dict.rs
@@ -1,11 +1,62 @@
 use std::collections::HashMap;
 
-use crate::ast::{Expr, LispResult};
+use itertools::Itertools;
+
+use crate::ast::{Expr, LispResult, SymbolTable};
+
+use super::RecordDoc;
 
 pub(crate) struct DictRecord(HashMap<Expr, Expr>);
 
 impl DictRecord {
-    fn init() -> LispResult<Self> {
-        todo!()
+    pub(crate) const RECORD_NAME: &'static str = "Dict";
+
+    fn init(items: Vec<Expr>, _symbol_table: &SymbolTable) -> LispResult<Self> {
+        anyhow::ensure!(
+            items.len() % 2 == 0,
+            "Dict requires an even list of arguments! Was given {:?}",
+            &items
+        );
+        Ok(DictRecord(items.into_iter().tuples().collect()))
+    }
+
+    fn display(&self) -> String {
+        format!(
+            "{}<{{{}}}>",
+            Self::RECORD_NAME,
+            self.0.iter().map(|(k, v)| format!("{}:{}", k, v)).join(" ")
+        )
+    }
+
+    fn get(&self, key: Expr) -> Option<Expr> {
+        self.0.get(&key).cloned()
+    }
+
+    fn contains(&self, key: Expr) -> bool {
+        self.0.contains_key(&key)
+    }
+
+    fn merge(&self, other: &Self) -> Self {
+        DictRecord(
+            self.0
+                .iter()
+                .chain(&other.0)
+                .map(|(k, v)| (k.clone(), v.clone()))
+                .collect(),
+        )
+    }
+}
+
+impl RecordDoc for DictRecord {
+    fn name() -> &'static str {
+        DictRecord::RECORD_NAME
+    }
+
+    fn type_doc() -> &'static str {
+        "Immutable dictionary."
+    }
+
+    fn method_doc() -> &'static [(&'static str, &'static str)] {
+        &[]
     }
 }
