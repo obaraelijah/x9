e91b3f81c50716bf60b4c95e1cad0912a452449d
obaraelijah <elijahobara357@gmail.com>
2024-06-12 21:58:57 +0300

    feat: Regex record

--- src/records/mod.rs
+++ src/records/mod.rs
@@ -2,8 +2,13 @@ mod dict;
 mod dyn_record;
 mod file;
 mod record;
+mod regex;
 mod set;
 mod tcp_socket;
 
 pub(crate) use self::record::{Record, RecordDoc, RecordType};
+pub(crate) use self::dict::DictRecord;
 pub(crate) use self::set::SetRecord;
+pub(crate) use self::dyn_record::DynRecord;
+pub(crate) use self::file::FileRecord;
+pub(crate) use self::tcp_socket::TcpListenerRecord;
---
+++ src/records/regex.rs
@@ -0,0 +1,35 @@
+use im::Vector;
+use regex::Regex;
+
+use crate::ast::{Expr, LispResult, SymbolTable};
+
+#[derive(Debug, Clone)]
+pub(crate) struct RegexRecord {
+    re: Regex,
+    regex_string: String,    
+}
+
+impl Default for RegexRecord {
+    fn default() -> Self {
+        Self {
+            re: Regex::new(".*").unwrap(),
+            regex_string: ".*".into(),
+        }
+    }
+}
+
+impl PartialEq for RegexRecord {
+    fn eq(&self, other: &Self) -> bool {
+        self.regex_string == other.regex_string
+    }
+}
+
+impl RegexRecord {
+    pub(crate) const RECORD_NAME: &'static str = "RegexRecord";
+
+    pub(crate) fn compile_x9(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
+        symbol_table
+            .lookup(&Self::RECORD_NAME.into())?
+            .call_fn(exprs, symbol_table)
+    }
+}
