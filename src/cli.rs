e288ccb883ecf837d210d82dacf9d7d95a684944
obaraelijah <elijahobara357@gmail.com>
2024-06-12 19:08:01 +0300

    feat:  Highligter for completions

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,7 @@ use rustyline::{
 };
 use rustyline_derive::Helper;
 use structopt::StructOpt;
+use std::borrow::Cow;
 
 use crate::ast::SymbolTable;
 
@@ -98,8 +99,24 @@ impl Highlighter for Completions {
         &'s self,
         prompt: &'p str,
         default: bool,
-    ) -> std::borrow::Cow<'b, str> {
-        todo!()
+    ) -> Cow<'b, str> {
+        if default {
+            Cow::Borrowed(&self.coloured_prompt)
+        } else {
+            Cow::Borrowed(prompt)
+        }
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Cow::Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize, forced: bool) -> bool {
+        self.highlighter.highlight_char(line, pos, forced)
     }
 }
 
