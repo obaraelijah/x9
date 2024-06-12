a8f13df8158e1cfeca13067dc69ace3bd9e7d3a7
obaraelijah <elijahobara357@gmail.com>
2024-06-12 13:50:10 +0300

    feat: Partial Eq for dynamic records

--- src/records/dyn_record.rs
+++ src/records/dyn_record.rs
@@ -1,5 +1,26 @@
-use crate::ast::Symbol;
+use dashmap::DashMap;
+use im::Vector;
 
+use crate::ast::{Expr, LispResult, Symbol, SymbolTable};
+use crate::records::{Record, RecordDoc};
+
+#[derive(Default, Debug, Clone)]
 pub struct DynRecord {
     name: Symbol,
+    fields: DashMap<Symbol, Expr>,
+    initialized: bool,
 }
+
+impl PartialEq for DynRecord {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+            && self.fields.iter().all(|kv| {
+                other
+                    .fields
+                    .get(kv.key())
+                    .map(|other_v| *other_v == *kv.value())
+                    .unwrap_or(false)
+            })
+    }
+}
+
--- src/records/mod.rs
+++ src/records/mod.rs
@@ -5,5 +5,5 @@ mod record;
 mod set;
 mod tcp_socket;
 
-pub(crate) use self::record::{Record, RecordType};
+pub(crate) use self::record::{Record, RecordType, RecordDoc};
 pub(crate) use self::set::SetRecord;
