f3ceaf258a7c58fea21fb6526d8642781fd8bf40
obaraelijah <elijahobara357@gmail.com>
2024-06-13 19:43:47 +0300

    feat: Dynamic record for records

--- src/records/dyn_record.rs
+++ src/records/dyn_record.rs
@@ -1,14 +1,16 @@
-use dashmap::DashMap;
-use im::Vector;
-
-use crate::ast::{Expr, LispResult, Symbol, SymbolTable};
 use crate::records::{Record, RecordDoc};
+use crate::ast::{Expr, Function, LispResult, Symbol, SymbolTable};
+use dashmap::DashMap;
+use std::sync::Arc;
 
 #[derive(Default, Debug, Clone)]
 pub struct DynRecord {
     name: Symbol,
+    doc: Option<String>,
     fields: DashMap<Symbol, Expr>,
     initialized: bool,
+    fields_order: Vec<Symbol>,
+    methods: Arc<DashMap<Symbol, Function>>,
 }
 
 impl PartialEq for DynRecord {
@@ -21,5 +23,19 @@ impl PartialEq for DynRecord {
                     .map(|other_v| *other_v == *kv.value())
                     .unwrap_or(false)
             })
+    }
+}
+
+impl RecordDoc for DynRecord {
+    fn name() -> &'static str {
+        "DynRecord"
+    }
+
+    fn type_doc() -> &'static str {
+        "Define a Record structure."
+    }
+
+    fn method_doc() -> &'static [(&'static str, &'static str)] {
+        &[]
     }
 }
