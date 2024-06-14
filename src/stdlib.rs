369585287ae75369e6e6000549b054a6e9a6c533
obaraelijah <elijahobara357@gmail.com>
2024-06-14 14:24:49 +0300

    feat: Std library arithmetics

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,3 +1,4 @@
+use bigdecimal::BigDecimal;
 use im::Vector;
 
 use crate::ast::{Expr, LispResult, SymbolTable};
@@ -50,4 +51,40 @@ fn and(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
         }
     }
     Ok(Expr::Bool(true))
+}
+
+fn xor(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
+    if !exprs.is_empty() {
+        let mut res = exprs[0].is_truthy(symbol_table)?;
+        for b in exprs.iter().skip(1) {
+            res ^= b.is_truthy(symbol_table)?;
+        }
+        Ok(Expr::Bool(res))
+    } else {
+        Ok(Expr::Bool(true))
+    }
+}
+
+fn not(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
+    // TODO get exact len
+    Ok(Expr::Bool(!exprs[0].is_truthy(symbol_table)?))
+}
+
+fn eq_exprs(exprs: Vector<Expr>, _symbol_table: &SymbolTable) -> LispResult<Expr> {
+    let first = &exprs[0];
+    let all_eq = exprs.iter().all(|x| first == x);
+    Ok(Expr::Bool(all_eq)) 
+}
+
+fn add_exprs(exprs: Vector<Expr>, _symbol_table: &SymbolTable) -> LispResult<Expr> {
+    let mut init = exprs[0].clone();
+    exprs.iter().skip(1).try_fold(init, |acc, x| acc + x)
+}
+
+fn sub_exprs(exprs: Vector<Expr>, _symbol_table: &SymbolTable) -> LispResult<Expr> {
+    let init = exprs[0].clone();
+    if exprs.len() == 1 {
+        return Ok(Expr::num(BigDecimal::from(-1) * init.get_num()?));
+    }
+    exprs.iter().skip(1).try_fold(init, |acc, x| acc - x)
 }
