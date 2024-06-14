dbb09ddce3dd6bab757d8c4e36f476fc151f242e
obaraelijah <elijahobara357@gmail.com>
2024-06-14 13:12:02 +0300

    feat: stdlib ops and & or

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -28,3 +28,26 @@ fn gte_exprs(exprs: Vector<Expr>, _symbol_table: &SymbolTable) -> LispResult<Exp
     let rest = exprs.iter().skip(1).all(|e| first >= e);
     Ok(Expr::Bool(rest))
 }
+
+fn rem_exprs(exprs: Vector<Expr>, _symbol_table: &SymbolTable) -> LispResult<Expr> {
+    // TODO: Check for exact length
+    exprs[0].clone() % &exprs[1]
+}
+
+fn or(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
+    for expr in exprs {
+        if expr.is_truthy(symbol_table)? {
+            return Ok(Expr::Bool(true));
+        }
+    }
+    Ok(Expr::Bool(false))
+}
+
+fn and(exprs: Vector<Expr>, symbol_table: &SymbolTable) -> LispResult<Expr> {
+    for expr in exprs {
+        if !expr.is_truthy(symbol_table)? {
+            return Ok(Expr::Bool(false));
+        }
+    }
+    Ok(Expr::Bool(true))
+}
