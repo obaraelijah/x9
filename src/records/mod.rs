7992cd30823d5d577d1b286e73971fc454a2c202
obaraelijah <elijahobara357@gmail.com>
2024-06-22 20:06:41 +0300

    chore: Register records

--- src/records/mod.rs
+++ src/records/mod.rs
@@ -7,9 +7,10 @@ mod set;
 mod struct_record;
 mod tcp_socket;
 
+pub(crate) use self::record::{Record, RecordDoc, RecordType};
+pub(crate) use self::regex::RegexRecord;
 pub(crate) use self::dict::DictRecord;
 pub(crate) use self::dyn_record::DynRecord;
 pub(crate) use self::file::FileRecord;
-pub(crate) use self::record::{Record, RecordDoc, RecordType};
 pub(crate) use self::set::SetRecord;
 pub(crate) use self::tcp_socket::TcpListenerRecord;
