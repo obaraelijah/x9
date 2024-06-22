cd79f9805d76da213859c49b76c3181387b1b26e
obaraelijah <elijahobara357@gmail.com>
2024-06-22 20:02:56 +0300

    feat: Bytecode vm

---
+++ src/vm/bytecode_vm.rs
@@ -0,0 +1,13 @@
+use crate::ast::SymbolTable;
+
+pub struct ByteCodeVM {
+    debug_mode: bool,
+}
+
+impl ByteCodeVM {
+    pub fn new(symbol_table: SymbolTable, debugger_flag: bool) -> Self {
+        ByteCodeVM {
+            debug_mode: debugger_flag
+        }
+    }
+}
