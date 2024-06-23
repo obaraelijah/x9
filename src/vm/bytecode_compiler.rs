bd40994f16476bf13c8b5505fafdc9d4d7eaf742
obaraelijah <elijahobara357@gmail.com>
2024-06-23 12:59:55 +0300

    feat: Register a label

--- src/vm/bytecode_compiler.rs
+++ src/vm/bytecode_compiler.rs
@@ -1,10 +1,20 @@
 use std::collections::HashMap;
 
-use crate::ast::ByteCompiledFunction;
+use anyhow::{anyhow, bail, ensure, Context};
+use im::{vector, Vector};
+use itertools::Itertools;
 
+use crate::{
+    bad_types, exact_len,
+    parser::read,
+    ast::{ByteCompiledFunction, Expr, LispResult, ProgramError, Symbol},
+};
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 struct Label(usize);
 
 pub struct ByteCodeCompiler {
+    instructions: Vec<String>,
     named_functions: Vec<(ByteCompiledFunction, Option<String>)>,
     label_map: HashMap<Label, usize>,
     label_count: usize,
@@ -19,6 +29,7 @@ impl Default for ByteCodeCompiler {
 impl ByteCodeCompiler {
     pub fn new() -> Self {
         ByteCodeCompiler {
+            instructions: Vec::new(),
             named_functions: Vec::new(),
             label_map: HashMap::new(),
             label_count: 0,
@@ -31,4 +42,15 @@ impl ByteCodeCompiler {
         Label(label)
     }
 
+    fn register_label(&mut self, label: Label) -> LispResult<()> {
+        if self.label_map.contains_key(&label) {
+            return Err(anyhow!("Label {:?} has already been registered!", label));
+        }
+        self.label_map.insert(label, self.len());
+        Ok(())
+    }
+
+    fn len(&self) -> usize {
+        self.instructions.len()
+    }
 }
