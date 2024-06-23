e534ef387124069ec0254e3bcbe82f42539c9fbd
obaraelijah <elijahobara357@gmail.com>
2024-06-23 12:49:45 +0300

    feat: Extend formatter & stdlib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use structopt::StructOpt;
-use x9::cli;
+use x9::{cli, formatter, stdlib, modules};
 
-fn main() -> Result<(), anyhow::Error> {
-    let _opt = cli::Options::from_args();
+fn main() -> Result<(), i32> {
+    let opt = cli::Options::from_args();
+    let mut sym_table = stdlib::create_stdlib_symbol_table(&opt);
+    if opt.formatter {
+        return formatter::format(&opt);
+    }
     Ok(())
 }
