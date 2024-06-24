923699edc16355ea79c148f8d8e11dc710078bf0
obaraelijah <elijahobara357@gmail.com>
2024-06-24 20:47:42 +0300

    feat: Add Criterion benchmarking

---
+++ benches/bench.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use x9::parser::read;
+
+fn parse_benchmark(c: &mut Criterion) {
+    let program = "(doall (take 100 (map fib (range)))) (+ 1 1)";
+    c.bench_function("parse doall", |b| {
+        b.iter(|| {
+            for i in read(&program) {
+                black_box(i.unwrap());
+            }
+        })
+    });
+}
+
+criterion_group!(name = benches;
+                 config = Criterion::default().sample_size(75);
+                 targets = parse_benchmark);
+criterion_main!(benches);
