8d6e954febae98b7a45e12abc81db4a8fa6f2a04
obaraelijah <elijahobara357@gmail.com>
2024-06-28 21:43:48 +0300

    feat: Record document

--- src/records/chan.rs
+++ src/records/chan.rs
@@ -1,3 +1,5 @@
+use super::RecordDoc;
+
 pub(crate) struct ReadChan {
     id: usize,
 }
@@ -15,9 +17,25 @@ pub(crate) struct WriteChan {
 }
 
 impl WriteChan {
+    pub(crate) const RECORD_NAME: &'static str = "WriteChan";
+
     fn new(self) -> Self {
         Self { 
             id: rand::random(),
         }
+    }
+}
+
+impl RecordDoc for WriteChan {
+    fn name() -> &'static str {
+        WriteChan::RECORD_NAME
+    }
+
+    fn type_doc() -> &'static str {
+        ""
+    }
+
+    fn method_doc() -> &'static [(&'static str, &'static str)] {
+        &[]
     }
 }
