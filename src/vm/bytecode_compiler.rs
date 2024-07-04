afdc39965cd41d900e110f078dab18e01383de43
obaraelijah <elijahobara357@gmail.com>
2024-07-04 14:47:00 +0300

    feat: Add parser2 -- an in-house parser that should be easier to extend

--- src/vm/bytecode_compiler.rs
+++ src/vm/bytecode_compiler.rs
@@ -57,6 +57,145 @@ impl ByteCodeCompiler {
         Ok(())
     }
 
+    fn compile_list(
+        &mut self,
+        sym: Option<&str>,
+        head: Expr,
+        args: Vector<Expr>,
+    ) -> LispResult<()> {
+        match sym {
+            Some("cond") => {
+                ensure!(args.len() % 2 == 0, ProgramError::CondBadConditionNotEven);
+                let end_jmp = self.new_label();
+                for (pred, body) in args.into_iter().tuples() {
+                    self.compile_expr(pred)?;
+                    let next_test_jump = self.new_label();
+                    self.push_test(next_test_jump);
+                    self.compile_expr(body)?;
+                    self.push_jmp(end_jmp);
+                    self.register_label(next_test_jump)?;
+                }
+                self.push_instruction(Instruction::Fail("Control flow reached past end of cond"));
+                self.register_label(end_jmp)?;
+            }
+            Some("def") => {
+                // TODO: Handle wrong number of args
+                exact_len!(args, 2);
+                let bind_sym = args[0].get_symbol()?;
+                self.compile_expr(args[1].clone())?;
+                self.push_instruction(Instruction::GlobalBind(bind_sym));
+                self.push_nil();
+            }
+            Some("defn") => {
+                exact_len!(args, 3, 4);
+                // (defn foo "doc" (x y) (+ x y))
+                let (name, doc, args, body) = if args.len() == 3 {
+                    (args[0].clone(), None, args[1].clone(), args[2].clone())
+                } else {
+                    (
+                        args[0].clone(),
+                        Some(args[1].get_string()?), // TODO: Why does this eval in stdlib :thonking_eval:
+                        args[2].clone(),
+                        args[3].clone(),
+                    )
+                };
+                let sym = name.get_symbol()?;
+                let ff = self.compile_function(Some(sym), args, body)?;
+                self.named_functions.push((ff, doc));
+                self.push_nil()
+                // TODO: Record the fact we have a function
+            }
+            Some("fn") => {
+                exact_len!(args, 2);
+                // (fn (x y) (+ x y))
+                let ff = self.compile_function(None, args[0].clone(), args[1].clone())?;
+                self.push_instruction(Instruction::Push(Expr::ByteCompiledFunction(ff)));
+            }
+            Some("head") => {
+                exact_len!(args, 1);
+                self.compile_expr(args[0].clone())?;
+                self.push_instruction(Instruction::Head);
+            }
+            Some("tail") => {
+                exact_len!(args, 1);
+                for arg in args.iter().rev() {
+                    self.compile_expr(arg.clone())?;
+                }
+                self.push_instruction(Instruction::Tail);
+            }
+            Some("cons") => {
+                exact_len!(args, 2);
+                for arg in args.iter().rev() {
+                    self.compile_expr(arg.clone())?;
+                }
+                self.push_instruction(Instruction::Cons);
+            }
+            Some("+") => {
+                for arg in args.iter().rev() {
+                    self.compile_expr(arg.clone())?;
+                }
+                self.push_instruction(Instruction::Add(args.len()));
+            }
+            Some("breakpoint") => {
+                exact_len!(args, 0);
+                self.push_instruction(Instruction::BreakPoint);
+                self.push_nil();
+            }
+            Some("map") => {
+                // (map fn coll)
+                dbg!(&args);
+                exact_len!(args, 2);
+                self.compile_expr(args[1].clone())?;
+                self.compile_expr(args[0].clone())?;
+                self.push_instruction(Instruction::Map);
+            }
+            _ => {
+                for arg in args.iter().rev() {
+                    self.compile_expr(arg.clone())?;
+                }
+                self.compile_expr(head)?;
+                self.push_instruction(Instruction::CallFn(args.len()));
+            }
+        };
+        Ok(())
+    }
+
+    fn compile_function(
+        &mut self,
+        sym: Option<Symbol>,
+        named_args: Expr,
+        body: Expr,
+    ) -> LispResult<ByteCompiledFunction> {
+        let end_of_fn = self.new_label();
+        self.push_jmp(end_of_fn);
+        let start_loc = self.len();
+        let arg_symbols: Vec<Symbol> = named_args
+            .get_list()?
+            .into_iter()
+            .map(|e| e.get_symbol())
+            .collect::<Result<_, _>>()?;
+        let minimum_args = arg_symbols.len();
+        self.push_instruction(Instruction::EnterScope);
+        for arg_sym in arg_symbols.iter().copied() {
+            self.push_instruction(Instruction::LocalScopeBind(arg_sym));
+        }
+        self.compile_expr(body)?;
+        self.push_instruction(Instruction::ExitScope);
+        self.push_instruction(Instruction::Ret);
+        let name = match sym {
+            Some(s) => s,
+            None => "AnonByteCompiledFn".into(),
+        };
+        let ff = ByteCompiledFunction::new(
+            name,
+            minimum_args,
+            arg_symbols.into_boxed_slice(),
+            start_loc,
+        );
+        self.register_label(end_of_fn)?;
+        Ok(ff)
+    }
+
     fn push_jmp(&mut self, label: Label) {
         self.instructions.push(UnlinkedInstruction::JumpTo(label))
     }
@@ -79,15 +218,65 @@ impl ByteCodeCompiler {
     }
 
     fn compile_expr(&mut self, e: Expr) -> LispResult<()> {
-        todo!()
+        if let Expr::List(l) = e {
+            if l.is_empty() {
+                self.push_instruction(Instruction::Push(Expr::List(vector![])));
+                return Ok(());
+            }
+            let head = &l[0];
+            match head {
+                Expr::Symbol(sym) => {
+                    let sym_str = sym.to_string();
+                    self.compile_list(Some(&sym_str), head.clone(), l.skip(1))?;
+                }
+                Expr::List(_) => {
+                    self.compile_list(None, head.clone(), l.skip(1))?;
+                }
+                _ => return bad_types!("uh oh"),
+            };
+        } else {
+            self.push_instruction(Instruction::Push(e));
+        }
+        Ok(())
     }
 
     fn link(&mut self) -> LispResult<Vec<Instruction>> {
-        todo!()
+        self.push_instruction(Instruction::Halt);
+        let mut output = Vec::with_capacity(self.instructions.len());
+        for instruction in self.instructions.iter().cloned() {
+            match instruction {
+                UnlinkedInstruction::Instruction(inst) => output.push(inst),
+                UnlinkedInstruction::JumpTo(label) => {
+                    let dest = self
+                        .label_map
+                        .get(&label)
+                        .ok_or_else(|| anyhow!("Unknown label {:?}", label))?;
+                    output.push(Instruction::Jump(*dest));
+                }
+                UnlinkedInstruction::Test(label) => {
+                    let dest = self
+                        .label_map
+                        .get(&label)
+                        .ok_or_else(|| anyhow!("Unknown label {:?}", label))?;
+                    output.push(Instruction::Test(*dest));
+                }
+            }
+        }
+        Ok(output)
     }
 
-    pub fn compile(&mut self, input: &str) -> LispResult<()> {
-        todo!()
+    pub fn compile(
+        &mut self,
+        input: &str,
+    ) -> LispResult<(
+        Vec<Instruction>,
+        Vec<(ByteCompiledFunction, Option<String>)>,
+    )> {
+        for expr in read(input) {
+            self.compile_expr(expr?)?;
+        }
+        let instructions = self.link()?;
+        Ok((instructions, self.named_functions.clone()))
     }
 }
 
