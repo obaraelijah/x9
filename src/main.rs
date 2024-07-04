74a40f904fd6670d7ddaba67d5cc810b39baced4
obaraelijah <elijahobara357@gmail.com>
2024-07-04 15:14:17 +0300

    feat: Extend byte-code compiler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#![allow(clippy::unnecessary_wraps)]
+
+use crate::cli::report_error;
 use structopt::StructOpt;
-use x9::{cli, formatter, modules, stdlib};
+
+use x9::{cli, formatter, modules, stdlib, vm::ByteCodeVM};
 
 fn main() -> Result<(), i32> {
     let opt = cli::Options::from_args();
@@ -7,5 +11,27 @@ fn main() -> Result<(), i32> {
     if opt.formatter {
         return formatter::format(&opt);
     }
+    if opt.files.is_empty() {
+        cli::read_cli(&sym_table, opt.byte_compile);
+    } else {
+        for f in opt.files {
+            if opt.byte_compile {
+                let contents = std::fs::read_to_string(&f).expect("Failed to read file");
+                if let Err(e) = ByteCodeVM::new(sym_table, opt.debugger).run(&contents) {
+                    report_error(&e);
+                    return Err(1);
+                }
+                return Ok(());
+            }
+            if let Err(e) = modules::run_file(&f, &sym_table) {
+                report_error(&e);
+                return Err(1);
+            }
+        }
+        if opt.load_file {
+            cli::read_cli(&sym_table, opt.byte_compile);
+        }
+    }
+    sym_table.wait_on_threads();
     Ok(())
 }
