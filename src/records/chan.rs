4900865fbeeb9fdceea41aa8b8df98ad5e2df5d8
obaraelijah <elijahobara357@gmail.com>
2024-07-04 14:18:22 +0300

    Add coroutines, go, and chan. General multithreading support

--- src/records/chan.rs
+++ src/records/chan.rs
@@ -33,7 +33,6 @@ impl ReadChan {
         }
     }
 
-    
     fn close(&mut self) {
         self.reader.lock().take();
     }
@@ -45,11 +44,10 @@ impl ReadChan {
     fn recv(&self) -> LispResult<Expr> {
         let guard = self.reader.lock();
         match *guard {
-            Some(ref reader) => reader.recv().map_err(|e| anyhow!("{e:?}")),
+            Some(ref reader) => reader.recv().map_err(|e| anyhow!("{:?}", e)),
             None => Err(anyhow!("Reader has been closed!")),
         }
     }
-
 
     fn display(&self) -> String {
         format!("SendChan<{:?}>", self.reader.lock())
@@ -62,11 +60,35 @@ impl RecordDoc for ReadChan {
     }
 
     fn type_doc() -> &'static str {
-        ""
+        "Read side of a channel"
     }
 
     fn method_doc() -> &'static [(&'static str, &'static str)] {
-        &[]
+        &[
+            (
+                "recv",
+                "Read some item from a channel. This will block until an item is received or the sender is closed. Example:
+
+;; `w` is some writer
+(bind
+ ((writer reader) (chan))
+ (do
+   (go (fn () (do (println (.recv r))))) ;; recv items
+   (.send writer \"item 1\")
+   (sleep 1)
+   (.send writer \"item 2\")
+   (.close writer) ;; \"item 1\" and \"item 2\" are printed
+  ))",
+            ),
+            (
+                "close",
+                "Close the reader. This will fail if the reader has been closed.",
+            ),
+            (
+                "is_closed",
+                "Returns true if the channel is closed."
+            )
+        ]
     }
 }
 
@@ -98,7 +120,7 @@ impl WriteChan {
             Some(ref sender) => sender
                 .send(item)
                 .map(|_| Expr::Nil)
-                .map_err(|e| anyhow!("Failed to send item into channel: {e:?}")),
+                .map_err(|e| anyhow!("Failed to send item into channel: {:?}", e)),
             None => Err(anyhow!("Attempted to send on a closed channel!")),
         }
     }
@@ -122,11 +144,24 @@ impl RecordDoc for WriteChan {
     }
 
     fn type_doc() -> &'static str {
-        ""
+        "Write side of a channel"
     }
 
     fn method_doc() -> &'static [(&'static str, &'static str)] {
-        &[]
+        &[
+            (
+                "send",
+                "Sent some item into a channel.
+;; `w` is some writer
+(.send w \"Some item 1\")
+",
+            ),
+            (
+                "close",
+                "Close the writer. This will stop any readers on the channel.",
+            ),
+            ("is_closed", "Returns true if the channel is closed."),
+        ]
     }
 }
 
